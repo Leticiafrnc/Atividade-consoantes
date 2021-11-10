72b10bc4a505241f5abd401c0d0be0040c1358d4
https://github.com/Leticiafrnc <leticiafrnc1@gmail.com>
2021-11-10 15:40:18 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,152 @@
+function conferirRato() {
+  var respR = document.getElementById("respR").value;
+  const respT = document.getElementById("respT").value
+
+  if (respR === "r" && respT === "t") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respostaRato").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respostaRato").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirBala() {
+  var respB = document.getElementById("respB").value;
+  const respL = document.getElementById("respL").value
+
+  if (respB === "b" && respL === "l") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respostaBala").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respostaBala").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirMola() {
+  var respMo = document.getElementById("respMo").value;
+  const respLa = document.getElementById("respLa").value
+
+  if (respMo === "m" && respLa === "l") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respostaMola").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respostaMola").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirBolo() {
+  var respBo = document.getElementById("respBo").value;
+  const respLo = document.getElementById("respLo").value
+
+  if (respBo === "b" && respLo === "l") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respostaBolo").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respostaBolo").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirBule() {
+  var respBu = document.getElementById("respBu").value;
+  const respLe = document.getElementById("respLe").value
+
+  if (respBu === "b" && respLe === "l") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respostaBule").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respostaBule").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirFogo() {
+  var respFo = document.getElementById("respFo").value;
+  const respGo = document.getElementById("respGo").value
+
+  if (respFo === "f" && respGo === "g") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respostaFogo").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respostaFogo").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirMala() {
+  var respMala = document.getElementById("respMala").value;
+  const respLal = document.getElementById("respLal").value
+
+  if (respMala === "m" && respLal === "l") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respostaMala").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respostaMala").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirRede() {
+  var respRe = document.getElementById("respRe").value;
+  const respDe = document.getElementById("respDe").value
+
+  if (respRe === "r" && respDe === "d") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respostaRede").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respostaRede").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirPato() {
+  var respPa = document.getElementById("respPa").value;
+  const respTo = document.getElementById("respTo").value
+
+  if (respPa === "p" && respTo === "t") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respostaPato").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respostaPato").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirCopo() {
+  var respCo = document.getElementById("respCo").value;
+  const respPo = document.getElementById("respPo").value
+
+  if (respCo === "c" && respPo === "p") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respostaCopo").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respostaCopo").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirDedo() {
+  var respDed = document.getElementById("respDed").value;
+  const respDo = document.getElementById("respDo").value
+
+  if (respDed === "d" && respDo === "d") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respostaDedo").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respostaDedo").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirRoda() {
+  var respRo = document.getElementById("respRo").value;
+  const resDa = document.getElementById("resDa").value
+
+  if (respRo === "r" && resDa === "d") // verifa se os dois são verdadeiros
+  {
+    document.getElementById("respostaRoda").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respostaRoda").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+
+
+
+
+
+
+
+
